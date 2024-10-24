c81dcaa0968cda0e7b75a5250b0b519bbf4be486
wqtb <62031610+wqtb@users.noreply.github.com>
2024-10-24 08:30:11 +0800

    Update verify.sh

--- 网络策略/step1/verify.sh
+++ 网络策略/step1/verify.sh
@@ -14,16 +14,16 @@ while IFS=',' read -ra pod_data; do
 
     # 验证 Pod 名称是否符合预期
     if [[ " ${expected_pod_names[*]} " == *" $pod_name "* ]]; then
-        echo "Pod name '$pod_name' matches expected."
+        exit 0
     else
-        echo "Pod name '$pod_name' does not match expected."
+        exit 1
     fi
 
     # 验证 Pod 标签是否符合预期
     if [[ " ${expected_pod_labels[*]} " == *" $pod_labels "* ]]; then
-        echo "Pod labels '$pod_labels' match expected."
+        exit 0
     else
-        echo "Pod labels '$pod_labels' do not match expected."
+        exit 1
     fi
 done <<< "$pod_info"
 
