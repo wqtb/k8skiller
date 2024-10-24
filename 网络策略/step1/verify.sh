5bfb9b4b862ed6e7472db907c57e35ea64688086
wqtb <62031610+wqtb@users.noreply.github.com>
2024-10-24 08:45:34 +0800

    Update verify.sh

--- 网络策略/step1/verify.sh
+++ 网络策略/step1/verify.sh
@@ -1,29 +1,39 @@
+
 #!/bin/bash
-
-# 获取所有 Pod 的名称和标签
 pod_info=$(kubectl get pods -o json | jq -r '.items[] | [.metadata.name, (.metadata.labels | to_entries[] | "\(.key)=\(.value)")]')
-
 # 定义预期的 Pod 名称和标签
 expected_pod_names=("approved-pod" "critical-pod" "test-pod")
 expected_pod_labels=("access=approved" "type=critical" "type=test")
 
-# 遍历每个 Pod 的信息
-while IFS=',' read -ra pod_data; do
+# 标志变量,用于跟踪验证结果
+verified=0
+
+# 遍历 $pod_info 数组
+for pod_info in "${pod_info[@]}"; do
+    # 将 pod_info 拆分为名称和标签
+    IFS=',' read -ra pod_data <<< "$pod_info"
     pod_name="${pod_data[0]}"
     pod_labels="${pod_data[1]}"
 
     # 验证 Pod 名称是否符合预期
-    if [[ " ${expected_pod_names[*]} " == *" $pod_name "* ]]; then
-        exit 0
+    if [[ "${expected_pod_names[*]}" == *"$pod_name"* ]]; then
+        :
     else
-        exit 1
+        verified=1
     fi
 
     # 验证 Pod 标签是否符合预期
-    if [[ " ${expected_pod_labels[*]} " == *" $pod_labels "* ]]; then
-        exit 0
+    if [[ "${expected_pod_labels[*]}" == *"$pod_labels"* ]]; then
+        :
     else
-        exit 1
+        verified=1
     fi
-done <<< "$pod_info"
+done
+
+# 根据验证结果返回退出状态
+if [ $verified -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
 
