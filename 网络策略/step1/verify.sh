cccbf031d9c0f7c07f8eaffbadb3f3061b76372d
wqtb <62031610+wqtb@users.noreply.github.com>
2024-10-24 08:07:41 +0800

    Update verify.sh

--- 网络策略/step1/verify.sh
+++ 网络策略/step1/verify.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
 
+kubectl get job k8skiller
+
+completions=$(kubectl get job k8skiller -o json | jq -r .spec.completions)
+parallelism=$(kubectl get job k8skiller -o json | jq -r .spec.parallelism)
+
+if [ "$completions" = "6" ] && [ "$parallelism" = "2" ]; then
+    exit 0
+else
+    exit 1
+fi
